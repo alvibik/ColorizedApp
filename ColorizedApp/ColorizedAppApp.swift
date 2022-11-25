b29d3d99acfc7be00002461e3787418e54611e0b
Александр Биктеев <bikteev@mail.ru>
2022-11-25 18:18:21 +0600

    Initial Commit

---
+++ ColorizedApp/ColorizedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedAppApp.swift
+//  ColorizedApp
+//
+//  Created by albik on 25.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
